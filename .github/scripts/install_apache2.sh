03ada32f98ae6f7e8b405b60244446a513f9f110
Sven Larson <svenlarsonsw@tutanota.com>
2020-02-27 20:54:39 +0100

    try without explicit php install

--- .github/scripts/install_apache2.sh
+++ .github/scripts/install_apache2.sh
@@ -5,15 +5,14 @@ sudo apt-get update
 sudo apt-get install -y --force-yes apache2
 
 # Install php 7.3
-sudo apt-get install libapache2-mod-php7.3
+# sudo apt-get install -y --force-yes libapache2-mod-php7.3
 
 # Copy website data
-sudo cp www/info.php /var/www/info.php
-sudo cp www/apache.php /var/www/apache.php
+sudo cp build/phpinfo.php /var/www/phpinfo.php
 
 # Enable mods
 sudo a2enmod rewrite
-sudo a2enmod php7.3
+#sudo a2enmod php7.3
 
 # Restart apache
 sudo service apache2 restart

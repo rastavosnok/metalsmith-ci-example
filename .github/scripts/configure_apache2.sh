b9073128eabefa63a797cea9f4816296488d54c8
Sven Larson <svenlarsonsw@tutanota.com>
2020-02-28 13:53:25 +0100

    .

---
+++ .github/scripts/configure_apache2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Copy website data
+sudo cp build/phpinfo.php /var/www/html/phpinfo.php
+
+# Enable mods
+sudo a2enmod rewrite
+sudo a2enmod php7.4
+
+# Restart apache
+sudo service apache2 restart
+
--- .github/scripts/install_apache2.sh
+++ .github/scripts/install_apache2.sh
@@ -5,15 +5,4 @@ sudo apt-get update
 sudo apt-get install -y --force-yes apache2
 
 # Install php 7.3
-# sudo apt-get install -y --force-yes libapache2-mod-php7.3
-
-# Copy website data
-sudo cp build/phpinfo.php /var/www/html/phpinfo.php
-
-# Enable mods
-sudo a2enmod rewrite
-sudo a2enmod php7.4
-
-# Restart apache
-sudo service apache2 restart
-
+sudo apt-get install -y --force-yes libapache2-mod-php7.4
